191e73972ad3b3de7f55b816a6cbb4626f344f05
mzidan25 <muhammadzidan25@student.ub.ac.id>
2024-08-25 23:23:59 +0700

    Initial Commit

---
+++ app/src/main/java/com/example/tugaspertamazidan/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.example.tugaspertamazidan.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+val OffWhite = Color(0xFFFAF9F6)
+val CustomBlack = Color(0xFF0D1C2C)
+val LightCustomBlack = Color(0xFF172A3D)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
